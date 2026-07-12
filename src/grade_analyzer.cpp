a1f0d2dca2e00562097e61156c24f6f9162cbd41
andrewchencc <achen026@student.sdccd.edu>
2026-07-12 21:06:56 +0000

    acpeerreview

--- src/grade_analyzer.cpp
+++ src/grade_analyzer.cpp
@@ -1,48 +1,65 @@
 #include "grade_analyzer.hpp"
 #include <iostream>
 
-using namespace std;
+// ac: delete using namespace std;
 
 double calculateTotal(double score1, double score2, double score3) {
+    // Return the sum of the three scores.
     return score1 + score2 + score3;
 }
 
 double calculateAverage(double total, int count) {
-    if (count <= 0) {
-        return 0.0;
-    }
-
-    return total / count;
+    // If count is less than or equal to 0, return 0.0.
+    // Otherwise, return total divided by count.
+    if (count <= 0){
+    return 0.0;
+    } //ac: removed unncessary else and cleaned formatting
+    return total/count;
 }
 
 char determineLetterGrade(double average) {
-    if (average >= 90.0) {
+    // Return 'A' for averages 90 or higher.
+    // Return 'B' for averages 80 or higher.
+    // Return 'C' for averages 70 or higher.
+    // Return 'D' for averages 60 or higher.
+    // Return 'F' otherwise.
+    if (average >= 90) {
         return 'A';
-    } else if (average >= 80.0) {
+    }
+    else if (average >= 80) {
         return 'B';
-    } else if (average >= 70.0) {
+    }
+    else if (average >= 70) {
         return 'C';
-    } else if (average >= 60.0) {
+    }
+    else if (average >= 60) {
         return 'D';
     }
-
-    return 'F';
+    else {
+        return 'F';
+    } 
 }
 
 bool isPassing(double average) {
-    return average >= 70.0;
+    //  Return true when average is 70 or higher.
+    if (average >= 70) {
+        return true;
+    }
+    return false;
 }
 
 bool isValidScore(double score) {
-    return score >= 0.0 && score <= 100.0;
+    // Return true when score is between 0 and 100, inclusive.
+    if ((score >= 0) && (score <= 100)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+   
 }
 
-void printGradeReport(
-    double total,
-    double average,
-    char letterGrade,
-    bool passing
-) {
+void printGradeReport(double total, double average, char letterGrade, bool passing) {
     cout << endl;
     cout << "Grade Report" << endl;
     cout << "Total: " << total << endl;
